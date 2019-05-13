12bf810984f196f9649ad4ddbb2901447770d6ba
adamjgordon28 <adamjgordon24@gmail.com>
2019-05-13 14:29:12 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
 import React from 'react';
-import logo from './logo.svg';
+import {EditorState, RichUtils, convertToRaw } from "draft-js";
+import Editor from 'draft-js-plugins-editor'
+import createEmojiPlugin from 'draft-js-emoji-plugin';
+import createHighlightPlugin from './highlightPlugin';
+import 'draft-js-emoji-plugin/lib/plugin.css'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const emojiPlugin = createEmojiPlugin();
+
+const { EmojiSuggestions } = emojiPlugin;
+
+const highlightPlugin = createHighlightPlugin({
+  background: 'purple'
+});
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty()
+    }
+  }
+
+  makeBold(){
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      'BOLD'
+    ))
+  }
+
+  makeItalic(){
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      'ITALIC'
+    ))
+  }
+
+  makeUnderlined(){
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      'UNDERLINE'
+    ))
+  }
+
+  makeHighlighted(){
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      'HIGHLIGHT'
+    ))
+  }
+
+
+  onChange =(editorState) => {
+    this.setState({
+      editorState: editorState
+    })
+  }
+
+  handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+
+    return 'not-handled';
+  }
+
+
+
+
+  render() {
+    const raw = convertToRaw(this.state.editorState.getCurrentContent())
+    return (
+    <div>
+    <button onClick={()=> {this.makeBold()}}>Bold</button>
+    <button onClick={()=> {this.makeItalic()}}>Italicize</button>
+    <button onClick={()=> {this.makeUnderlined()}}>Underline</button>
+    <button onClick={()=> {this.makeHighlighted()}}>Highlight</button>
+    <Editor
+    onChange={(editorState) => {this.onChange(editorState)}}
+    handleKeyCommand={this.handleKeyCommand}
+    editorState={this.state.editorState}
+    plugins={[highlightPlugin, emojiPlugin]}
+    />
+    <EmojiSuggestions />
     </div>
-  );
+  )};
+
 }
 
 export default App;
